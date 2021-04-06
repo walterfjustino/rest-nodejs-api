c88cc54b96f71f3417e17f047e65837b4e23c166
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-04-06 20:25:57 -0300

    Created POST route, add bodyParser library

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,11 +1,19 @@
-/*configurar o qualquer coisa dentro do express*/
+/*configurar qualquer coisa dentro do express*/
 
 const express = require('express')
 
 const consign = require('consign')
 
+const bodyParser = require('body-parser')
+
     module.exports = () => {
         const app = express() //cria a variavel app
+
+        /*adicionando bibliotecas*/
+        /*UTILIZADO PARA RECEBER DADOS VIA FORMULARIO DE BROWSERS */
+        app.use(bodyParser.urlencoded({extended: true})) ;
+       /*UTILIZADO PARA RECEBER DADOS VIA JSON */
+        app.use(bodyParser.json())     
 
         consign()           //configura a variavel app
             .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,6 +3,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos, e esta realizando um GET'))
 
-    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimento, e esta realizando um POST!'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+
+     res.send('Você está na rota de atendimento, e esta realizando um POST!')})
 
 }
