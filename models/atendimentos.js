7fe5008f5d16e6fada130a5a48c21d1c8a1af870
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-04-07 01:52:35 -0300

    Refactor table Atendimentos, included library moment

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,11 +1,17 @@
+const moment = require('moment')
+
 const conection = require('../infraestructure/conection')
 
 
 class Atendimento {
     adiciona(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
         const sql = 'INSERT INTO Atendimentos SET ?' /* ? = SIGNIFICA QUE IRÁ SER INSERIDO QUALQUER OBJETO NA TABELA ATENDIMENTOS*/
 
-        conection.query(sql, atendimento, (erro, resultados) => { /* dentro de QUERY pode ser passado varios parametros */
+        conection.query(sql, atendimentoDatado, (erro, resultados) => { /* dentro de QUERY pode ser passado varios parametros */
             if(erro){
                 console.log(erro)
             } else{
