ecdcfe53a84139b8d94051de95c9872f2dc162de
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-04-07 00:36:34 -0300

    Persisting data in database

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,7 +5,7 @@ class Atendimento {
     adiciona(atendimento){
         const sql = 'INSERT INTO Atendimentos SET ?' /* ? = SIGNIFICA QUE IRÁ SER INSERIDO QUALQUER OBJETO NA TABELA ATENDIMENTOS*/
 
-        conection.query(sql, atendimento, (erro, resultados) => {
+        conection.query(sql, atendimento, (erro, resultados) => { /* dentro de QUERY pode ser passado varios parametros */
             if(erro){
                 console.log(erro)
             } else{
